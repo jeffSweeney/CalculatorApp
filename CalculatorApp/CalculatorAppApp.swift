4191b7e740e9edba0dfef28a03e19d22e3779d89
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2022-01-16 14:23:36 -0500

    Initial Commit

---
+++ CalculatorApp/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  CalculatorApp
+//
+//  Created by Jeffrey Sweeney on 1/16/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
